6fdea81049b15def5e37e8130f93f3af8ba92aef
Philips Nge <5889921+sudophils@users.noreply.github.com>
2024-06-24 08:57:03 -0400

    update

---
+++ android/src/main/kotlin/com/phixlab/app_activity_trace/AppActivityTrace.kt
@@ -0,0 +1,8 @@
+package com.phixlab.app_activity_trace;
+import io.flutter.plugin.common.PluginRegistry
+
+object AppActivityTrace {
+    fun registerWith(registrar: PluginRegistry.Registrar?) {
+        // Plugin-specific registration logic
+    }
+}
